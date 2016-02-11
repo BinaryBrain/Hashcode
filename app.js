5c71d3ad5cef41bb9468274f5db19deacf31ea2a
BafS <fabacrans@hotmail.com>
2016-02-11 19:32:18 +0100

    Init.

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const fs = require('fs');
+
+let input = 'input_exemple.txt'
+
+class Map {
+
+}
+
+let data = fs.readFileSync(input).toString().split("\n")
+
+console.log(data);
+
+let end = false
+while (!end) {
+
+}
