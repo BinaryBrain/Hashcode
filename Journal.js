e617355c2fe8b012e8f99273bca55ecd0f008357
Sacha Bron <sacha.bron@copperflake.com>
2016-02-11 20:22:53 +0100

    Fix and test Journal

--- Journal.js
+++ Journal.js
@@ -1,27 +1,27 @@
 'use strict';
 
 let Journal = {
-	commands = [];
+	commands: [],
 
 	getOutput: function () {
 		// Q, the number of drone commands
-		let output = commands.length + '\n' + commands.join('\n');
+		return this.commands.length + '\n' + this.commands.join('\n');
 	},
 
 	load: function (droneId, warehouseId, productTypeId, numberOfItem) {
-		commands.push(`${droneId} L ${warehouseId} ${productTypeId} ${numberOfItem}`);
+		this.commands.push(`${droneId} L ${warehouseId} ${productTypeId} ${numberOfItem}`);
 	},
 	
 	unload: function (droneId, warehouseId, productTypeId, numberOfItem) {
-		commands.push(`${droneId} U ${warehouseId} ${productTypeId} ${numberOfItem}`);
+		this.commands.push(`${droneId} U ${warehouseId} ${productTypeId} ${numberOfItem}`);
 	},
 	
 	deliver: function (droneId, customerId, productTypeId, numberOfItem) {
-		commands.push(`${droneId} D ${customerId} ${productTypeId} ${numberOfItem}`);
+		this.commands.push(`${droneId} D ${customerId} ${productTypeId} ${numberOfItem}`);
 	},
 	
 	wait: function (droneId, numberOfTurn) {
-		commands.push(`${droneId} W ${numberOfTurn}`);
+		this.commands.push(`${droneId} W ${numberOfTurn}`);
 	}
 }
 
