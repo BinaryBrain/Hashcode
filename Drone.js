6e14bce090ab640de615d30bd651f5a7ddfe8e97
Kewin Dousse <kewin.d@websud.ch>
2016-02-11 22:09:45 +0100

    Correcting Drone's main logic

--- Drone.js
+++ Drone.js
@@ -12,7 +12,9 @@ class Drone {
 		this.currentOrder = this.world.orders.pop();
 		this.currentCommand = [];
 		this.remainingTicks = -1;
-		this.tickNb;
+		this.currentItemsId = 0;
+		this.currentItemsCount = 0;
+		this.tickNb = 0;
 		this.world = world;
 	}
 
@@ -31,7 +33,7 @@ class Drone {
 	}
 
 	computeNextStep() {
-		if (this.order.isComplete()) {
+		if (this.currentOrder.isComplete()) {
 			if (this.world.orders.length == 0) {
 				// DRONE FINISHED ITS WORK :)
 				var nbToWait = this.world.turns - this.tickNb;
@@ -39,12 +41,12 @@ class Drone {
 				this.remainingTicks = nbToWait;
 				wait(nbToWait);
 			} else {
-				this.order = this.world.orders.pop();
+				this.currentOrder = this.world.orders.pop();
 				// LOAD THE NEXT ORDER
 			}
 		}
 		if (this.items.length == 0) {
-			var required = this.order.getRequired();
+			var required = this.currentOrder.getRequired();
 			// Looking for an uncompleted type
 			for (var type in required) {
 				if (required[type] == 0) {
@@ -54,21 +56,40 @@ class Drone {
 				var warehouses = this.world.warehouses;
 				for (var warehouse in warehouses) {
 					// Counting number of items in that warehouse
-					var count = 0;
+					var available = 0;
 					for (var item in warehouse.items) {
 						if (item == type) {
-							count++;
+							available++;
 						}
 					}
-					if (count > 0) {
+					if (available > 0) {
+						var nbToTake = Math.min(available, required[type]);
 						// SET ORDER TO GO TO THAT WAREHOUSE
+						this.remainingTicks = dist(this.location, warehouse.location);
+						this.currentCommand = ["LOAD", warehouse.id, type, nbToTake];
+						this.location = warehouse.location;
+						this.currentWeight = type * Item.getWeight(type);
+						this.currentItemsId = type;
+						this.currentItemsCount = nbToTake;
+						// CREATE ITEMS LIST
+						var itemsList = [];
+						for (var i = 0; i < nbToTake; i++) {
+							itemsList.push(type);
+						}
+						load(itemsList);
+						warehouse.remove(itemsList);
 						break;
 					}
 				}
 			}
-			this.
 		} else {
 			// Set command to DELIVER to the order's position
+			this.currentCommand = ["DELIVER", this.currentOrder.position, this.currentItemsId, this.currentItemsCount];
+			this.remainingTicks = dist(this.location, this.currentOrder.position);
+			this.currentWeight = 0;
+			this.currentItemsId = -1;
+			this.currentItemsCount = 0;
+			this.location = this.currentOrder.position;
 		}
 	}
 
@@ -128,7 +149,7 @@ class Drone {
 
 	travel(dest) {
 		this.dest = dest;
-		this.tickToGo = dist(this.location, this.dest);
+		this.remainingTicks = dist(this.location, this.dest);
 	}
 }
 
