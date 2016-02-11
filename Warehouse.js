d54ee319d671ad5f2b10c832095298d23b607c48
BafS <fabacrans@hotmail.com>
2016-02-11 21:13:22 +0100

    Add "remove"

--- Warehouse.js
+++ Warehouse.js
@@ -5,6 +5,32 @@ class Warehouse {
 		this.location = location;
 		this.items = items;
 	}
+
+	/**
+	 * Remove item(s)
+	 * @param  {Array|Int} items
+	 */
+	remove(items) {
+
+		if (Array.isArray(items)) {
+			items.forEach(item => this.remove(item))
+		} else {
+
+			let found = false;
+			for (let i = 0; i < this.items; ++i) {
+				if (this.items[i] === item[i]) {
+					this.items[i].splice(i, 1)
+					found = true
+					break
+				}
+			}
+
+			if (!found) {
+				console.error('Item not found.')
+			}
+		}
+
+	}
 }
 
 module.exports = Warehouse;
